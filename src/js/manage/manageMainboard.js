d3ad948f3f282c0c6d7e91b78052956be39c343f
vvbvb <tgp26062010@gmail.com>
2025-08-25 20:40:29 +0700

    complete mainboard

--- src/js/manage/manageMainboard.js
+++ src/js/manage/manageMainboard.js
@@ -41,32 +41,33 @@ function loadMainboard() {
               <div class="col-md-10">
                   <h5>${mainboardData.title}</h5>
                   <p>ID: ${mainboardId}</p>
-                  <p>Chipset: ${mainboardData.chipset}</p>
+                  <p>Name: ${mainboardData.name}</p>
                   <p>Price: ${formattedPrice}</p>
-                  <p>Length: ${mainboardData.length}</p>
-                  <p>Memory: ${mainboardData.memory}</p>
-                  <p>Core Clock: ${mainboardData.coreClock}</p>
+                  <p>Memory Slots: ${mainboardData.memorySlots}</p>
+                  <p>Memory Max: ${mainboardData.memoryMax}</p>
+                  <p>Form Factor: ${mainboardData.formFactor}</p>
+                  <p>CPU Socket: ${mainboardData.cpuSocket}</p>
                   <p>Description: ${mainboardData.description}</p>
                   <p>Rating: ${mainboardData.rating}</p>
-                  <button class="btn btn-warning gpu-edit-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEdit" aria-expanded="false" aria-controls="collapseEdit" data-id="${mainboardId}">Edit GPU</button>
+                  <button class="btn btn-warning mainboard-edit-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEdit" aria-expanded="false" aria-controls="collapseEdit" data-id="${mainboardId}">Edit Mainboard</button>
                     <div class="collapse" id="collapseEdit">
                       <div class="card card-body">
-                        <form id="edit-gpu-form">
-                          <input type="hidden" id="edit-gpu-id" value="${mainboardId}" />
-                          <input type="text" id="edit-gpu-title" class="form-control mb-2" placeholder="Title" value="${mainboardData.title}" />
-                          <input type="text" id="edit-gpu-chipset" class="form-control mb-2" placeholder="Chipset" value="${mainboardData.chipset}" />
-                          <input type="number" id="edit-gpu-price" class="form-control mb-2" placeholder="Price" value="${mainboardData.price}" />
-                          <input type="text" id="edit-gpu-length" class="form-control mb-2" placeholder="Length" value="${mainboardData.length}" />
-                          <input type="text" id="edit-gpu-memory" class="form-control mb-2" placeholder="Memory" value="${mainboardData.memory}" />
-                          <input type="text" id="edit-gpu-core-clock" class="form-control mb-2" placeholder="Core Clock" value="${mainboardData.coreClock}" />
-                          <input type="number" id="edit-gpu-rating" class="form-control mb-2" placeholder="Rating" value="${mainboardData.rating}" />
-                          <textarea id="edit-gpu-description" class="form-control mb-2" placeholder="Description">${mainboardData.description}</textarea>
-                          <input type="file" id="edit-gpu-image" class="form-control mb-2" />
+                        <form id="edit-mainboard-form">
+                          <input type="text" id="edit-mainboard-title" class="form-control mb-2" placeholder="Title" value="${mainboardData.title}"/>
+                          <input type="text" id="edit-mainboard-name" class="form-control mb-2" placeholder="Name" value="${mainboardData.name}"/>
+                          <input type="number" id="edit-mainboard-price" class="form-control mb-2" placeholder="Price" value="${mainboardData.price}"/>
+                          <input type="number" id="edit-mainboard-memory-slots" class="form-control mb-2" placeholder="Memory Slots" value="${mainboardData.memorySlots}"/>
+                          <input type="text" id="edit-mainboard-memory-max" class="form-control mb-2" placeholder="Memory Max" value="${mainboardData.memoryMax}"/>
+                          <input type="text" id="edit-mainboard-form-factor" class="form-control mb-2" placeholder="Form Factor" value="${mainboardData.formFactor}"/>
+                          <input type="text" id="edit-mainboard-cpu-socket" class="form-control mb-2" placeholder="CPU Socket" value="${mainboardData.cpuSocket}"/>
+                          <textarea id="edit-mainboard-description" class="form-control mb-2" placeholder="Description" rows="3">${mainboardData.description}</textarea>
+                          <input type="number" id="edit-mainboard-rating" class="form-control mb-2" placeholder="Rating" value="${mainboardData.rating}"/>
+                          <input type="file" id="edit-mainboard-image" class="form-control mb-2" placeholder="Image" value="${mainboardData.imageUrl}"/>
                           <button type="submit" class="btn btn-primary">Save Changes</button>
                         </form>
                       </div>
                     </div>
-                  <button class="btn btn-danger gpu-delete-btn" data-id="${mainboardId}">Delete GPU</button>
+                  <button class="btn btn-danger mainboard-delete-btn" data-id="${mainboardId}">Delete Mainboard</button>
               </div>
             </div>
           </li>
@@ -74,44 +75,55 @@ function loadMainboard() {
       });
       mainboardList.innerHTML += htmls;
 
-      const btnDelete = document.querySelectorAll(".gpu-delete-btn");
+      const btnDelete = document.querySelectorAll(".mainboard-delete-btn");
       btnDelete.forEach((btn) => {
         btn.addEventListener("click", () => {
           const productId = btn.getAttribute("data-id");
-          deleteGPU(productId);
+          deleteMainboard(productId);
         });
       });
-      const btnEdit = document.querySelectorAll(".gpu-edit-btn");
+      const btnEdit = document.querySelectorAll(".mainboard-edit-btn");
       btnEdit.forEach((btn) => {
         btn.addEventListener("click", () => {
           const productId = btn.getAttribute("data-id");
-          editGPU(productId);
+          editMainboard(productId);
         });
       });
     })
     .catch((error) => {
-      htmls += `<li class="list-group-item">Error fetching GPU data: ${error.message}</li>`;
+      htmls += `<li class="list-group-item">Error fetching mainboard data: ${error.message}</li>`;
       mainboardList.innerHTML += htmls;
-      console.error("Error fetching GPU data:", error);
+      console.error("Error fetching mainboard data:", error);
     });
 }
 
-gpuForm.addEventListener("submit", async (e) => {
+mainboardForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const gpuTitle = document.getElementById("gpu-title").value;
-  const gpuChipset = document.getElementById("gpu-chipset").value;
-  const gpuPrice = document.getElementById("gpu-price").value;
-  const gpuImage = document.getElementById("gpu-image").files[0];
-  const gpuLength = document.getElementById("gpu-length").value;
-  const gpuMemory = document.getElementById("gpu-memory").value;
-  const gpuCoreClock = document.getElementById("gpu-core-clock").value;
-  const gpuDescription = document.getElementById("gpu-description").value;
-  const gpuRating = 0; // Default rating
-  const gpuId = await generateNextGPUId();
+  const mainboardTitle = document.getElementById("mainboard-title").value;
+  const mainboardName = document.getElementById("mainboard-name").value;
+  const mainboardPrice = document.getElementById("mainboard-price").value;
+  const mainboardImage = document.getElementById("mainboard-image").files[0];
+  const mainboardMemorySlots = document.getElementById(
+    "mainboard-memory-slots"
+  ).value;
+  const mainboardMemoryMax = document.getElementById(
+    "mainboard-memory-max"
+  ).value;
+  const mainboardFormFactor = document.getElementById(
+    "mainboard-form-factor"
+  ).value;
+  const mainboardCPUSocket = document.getElementById(
+    "mainboard-cpu-socket"
+  ).value;
+  const mainboardDescription = document.getElementById(
+    "mainboard-description"
+  ).value;
+  const mainboardRating = 0; // Default rating
+  const mainboardId = await generateNextMainboardId();
 
   let formData = new FormData();
-  if (gpuImage) {
-    formData.append("image", gpuImage);
+  if (mainboardImage) {
+    formData.append("image", mainboardImage);
   }
 
   fetch("https://server-file-upload.onrender.com/upload", {
@@ -122,97 +134,108 @@ gpuForm.addEventListener("submit", async (e) => {
     .then((result) => {
       return db
         .collection("mainboardData")
-        .doc(gpuId)
+        .doc(mainboardId)
         .set({
-          id: gpuId,
-          title: gpuTitle,
-          chipset: gpuChipset,
-          price: parseFloat(gpuPrice),
+          id: mainboardId,
+          title: mainboardTitle,
+          name: mainboardName,
+          price: parseFloat(mainboardPrice),
           imageUrl: result.data.secure_url,
-          length: gpuLength,
-          memory: gpuMemory,
-          coreClock: gpuCoreClock,
-          description: gpuDescription,
-          rating: gpuRating,
+          memorySlots: mainboardMemorySlots,
+          memoryMax: mainboardMemoryMax,
+          formFactor: mainboardFormFactor,
+          cpuSocket: mainboardCPUSocket,
+          description: mainboardDescription,
+          rating: mainboardRating,
         });
     })
     .then(() => {
-      alert("GPU added successfully!");
-      gpuForm.reset();
+      alert("Mainboard added successfully!");
+      mainboardForm.reset();
       window.location.reload();
     })
     .catch((error) => {
-      console.error("Error adding GPU:", error);
+      console.error("Error adding mainboard:", error);
     });
 });
 
-function deleteGPU(gpuId) {
+function deleteMainboard(mainboardId) {
   if (confirm("Are you sure you want to delete this GPU?")) {
-    db.collection("gpuData")
-      .doc(gpuId)
+    db.collection("mainboardData")
+      .doc(mainboardId)
       .delete()
       .then(() => {
-        console.log("GPU deleted successfully!");
+        console.log("Mainboard deleted successfully!");
         window.location.reload();
       })
       .catch((error) => {
-        console.error("Error deleting GPU:", error);
+        console.error("Error deleting mainboard:", error);
       });
   }
 }
 
-function editGPU(gpuId) {
-  const editForm = document.getElementById("edit-gpu-form");
+function editMainboard(mainboardId) {
+  const editForm = document.getElementById("edit-mainboard-form");
 
   editForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    const gpuTitle = document.getElementById("edit-gpu-title").value;
-    const gpuChipset = document.getElementById("edit-gpu-chipset").value;
-    const gpuPrice = document.getElementById("edit-gpu-price").value;
-    const gpuImageFile = document.getElementById("edit-gpu-image").files[0];
-    const gpuLength = document.getElementById("edit-gpu-length").value;
-    const gpuMemory = document.getElementById("edit-gpu-memory").value;
-    const gpuCoreClock = document.getElementById("edit-gpu-core-clock").value;
-    const gpuDescription = document.getElementById(
-      "edit-gpu-description"
+    const mainboardTitle = document.getElementById("edit-mainboard-title").value;
+    const mainboardName = document.getElementById("edit-mainboard-name").value;
+    const mainboardPrice = document.getElementById("edit-mainboard-price").value;
+    const mainboardImageFile = document.getElementById("edit-mainboard-image").files[0];
+    const mainboardMemorySlots = document.getElementById(
+      "edit-mainboard-memory-slots"
     ).value;
-    const gpuRating = 0; // Default rating
+    const mainboardMemoryMax = document.getElementById(
+      "edit-mainboard-memory-max"
+    ).value;
+    const mainboardFormFactor = document.getElementById(
+      "edit-mainboard-form-factor"
+    ).value;
+    const mainboardCPUSocket = document.getElementById(
+      "edit-mainboard-cpu-socket"
+    ).value;
+    const mainboardDescription = document.getElementById(
+      "edit-mainboard-description"
+    ).value;
+    const mainboardRating = document.getElementById("edit-mainboard-rating").value; // Default rating
 
     let updateData = {
-      title: gpuTitle,
-      chipset: gpuChipset,
-      price: parseFloat(gpuPrice),
-      length: gpuLength,
-      memory: gpuMemory,
-      coreClock: gpuCoreClock,
-      description: gpuDescription,
-      rating: gpuRating,
+          title: mainboardTitle,
+          name: mainboardName,
+          price: parseFloat(mainboardPrice),
+          memorySlots: mainboardMemorySlots,
+          memoryMax: mainboardMemoryMax,
+          formFactor: mainboardFormFactor,
+          cpuSocket: mainboardCPUSocket,
+          description: mainboardDescription,
+          rating: parseFloat(mainboardRating),
     };
 
     // Hàm update vào Firestore
-    function updateGPU(imageUrl) {
+    function updateMainboard(imageUrl) {
       if (imageUrl) {
         updateData.imageUrl = imageUrl;
       }
 
-      db.collection("gpuData")
-        .doc(gpuId)
+      db.collection("mainboardData")
+        .doc(mainboardId)
         .update(updateData)
         .then(function () {
-          alert("GPU updated successfully!");
-          console.log("GPU updated successfully!");
+          alert("Mainboard updated successfully!");
+          console.log("Mainboard updated successfully!");
           window.location.reload();
         })
         .catch(function (error) {
-          console.error("Error updating GPU:", error);
-          alert("Có lỗi khi cập nhật case, vui lòng thử lại!");
+          console.error("Error updating mainboard:", error);
+          alert("Có lỗi khi cập nhật mainboard, vui lòng thử lại!");
         });
     }
 
     // Nếu có ảnh thì upload trước, rồi mới update Firestore
-    if (gpuImageFile) {
+    if (mainboardImageFile) {
       let formData = new FormData();
-      formData.append("image", gpuImageFile);
+      formData.append("image", mainboardImageFile);
 
       fetch("https://server-file-upload.onrender.com/upload", {
         method: "POST",
@@ -221,7 +244,7 @@ function editGPU(gpuId) {
         .then((res) => res.json())
         .then((result) => {
           if (result && result.data && result.data.secure_url) {
-            updateGPU(result.data.secure_url); // update Firestore kèm ảnh
+            updateMainboard(result.data.secure_url); // update Firestore kèm ảnh
           } else {
             throw new Error("Không nhận được secure_url từ server upload!");
           }
@@ -232,7 +255,7 @@ function editGPU(gpuId) {
         });
     } else {
       // Nếu không có ảnh thì update luôn
-      updateGPU();
+      updateMainboard();
     }
   });
 }
